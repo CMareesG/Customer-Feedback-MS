27900421cddd4568124901dbc52386466fcd2bba
Poornisha T <poornisha.t@jmangroup.com>
2026-02-19 18:16:25 +0530

    Initial commit for notification

--- customer-feedback-backend/src/app.module.ts
+++ customer-feedback-backend/src/app.module.ts
@@ -3,9 +3,10 @@ import { AppController } from './app.controller';
 import { AppService } from './app.service';
 import { FeedbackModule } from './feedback/feedback.module';
 import { PrismaModule } from './prisma/prisma.module';
+import { NotificationModule } from './notification/notification.module';
 
 @Module({
-  imports: [FeedbackModule, PrismaModule, PrismaModule],
+  imports: [FeedbackModule, PrismaModule, PrismaModule, NotificationModule],
   controllers: [AppController],
   providers: [AppService],
 })
