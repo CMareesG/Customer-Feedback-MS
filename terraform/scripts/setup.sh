490a8b2f64425f406a478c0a16419eb7139ca52f
J Udhayashankar <udhayashankar@jmangroup.com>
2026-03-11 12:44:59 +0530

    Updated scripts

--- terraform/scripts/setup.sh
+++ terraform/scripts/setup.sh
@@ -1,20 +1,27 @@
 #!/bin/bash
-
-sudo apt-get update -y
-
-curl -fsSL https://get.docker.com | sudo sh
-
-sudo systemctl start docker
-sudo systemctl enable docker
-
-sudo apt-get install -y docker-compose
-
-sudo apt-get install -y git
+apt-get update
+apt-get install -y docker.io docker-compose
+systemctl start docker
+systemctl enable docker
+usermod -aG docker testadmin
 
 cd /home/testadmin
+git clone -b prod https://github.com/CMareesG/Customer-Feedback-MS.git
 
-git clone -b dev https://github.com/CMareesG/Customer-Feedback-MS.git
+cd /home/testadmin/Customer-Feedback-MS
 
-cd Customer-Feedback-MS
+DATABASE_URL=$(az keyvault secret show --vault-name kvcfms --name database-url --query value -o tsv)
+JWT_SECRET=$(az keyvault secret show --vault-name kvcfms --name jwt-secret --query value -o tsv)
+JWT_EXPIRES_IN=$(az keyvault secret show --vault-name kvcfms --name jwt-expires-in --query value -o tsv)
+REFRESH_EXPIRES_IN=$(az keyvault secret show --vault-name kvcfms --name refresh-expires-in --query value -o tsv)
+
+cd /home/testadmin/Customer-Feedback-MS/customer-feedback-backend
+
+echo "DATABASE_URL=$DATABASE_URL" > .env
+echo "JWT_SECRET=$JWT_SECRET" >> .env
+echo "JWT_EXPIRES_IN=$JWT_EXPIRES_IN" >> .env
+echo "REFRESH_EXPIRES_IN=$REFRESH_EXPIRES_IN" >> .env
+
+cd /home/testadmin/Customer-Feedback-MS
 
 sudo docker-compose up -d
